e4b3e1c27736d2ded9ecd3b1951ef61083515920
Chris Seymour <christopher.seymour@nanoporetech.com>
2019-05-09 18:53:44 +0100

    lower version for testing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pystreamvbyte',
-    version='0.4.1',
+    version='0.0.1',
     packages=find_packages(),
     install_requires=['numpy', 'cffi'],
     setup_requires=['cffi', 'wheel'],
