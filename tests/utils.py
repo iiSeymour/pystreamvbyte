e776868ba9d6f557089ce8d4ef0e0471bc0a9dfa
Chris Seymour <christopher.seymour@nanoporetech.com>
2019-05-15 16:57:26 +0100

    ensure dtype is passed to decode (not str)

--- tests/utils.py
+++ tests/utils.py
@@ -7,6 +7,28 @@ def generate_data(rmax=256, size=int(1e5), dtype=np.uint32):
     return np.random.randint(0, rmax, size=size, dtype=dtype)
 
 
+def convert_bytes(length, suffix="B"):
+    """
+    Return bytes in human readable format.
+
+    >>> convert_bytes(10)
+    '10 B'
+    >>> convert_bytes(10001)
+    '10.00 KB'
+    >>> convert_bytes(23123456789)
+    '23.12 GB'
+    """
+    return "%6.3f %s%s" % (length / 1e9, 'G', suffix)
+
+    for idx, unit in enumerate(['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']):
+        if abs(length) < 1000.0:
+            if idx == 0:
+                return "%6.0f %s%s" % (length, unit, suffix)
+            else:
+                return "%6.2f %s%s" % (length, unit, suffix)
+        length /= 1000.0
+
+
 @contextmanager
 def time(name):
     t0 = perf_counter()
@@ -16,9 +38,10 @@ def time(name):
 
 
 @contextmanager
-def benchmark(name, size):
+def benchmark(name, size, dtype):
     t0 = perf_counter()
     yield
     t1 = perf_counter() - t0
     print("%s time %.6f s," % (name, t1), end=" ")
-    print("{:,} uints/sec".format(int(size / t1)))
+    nbytes = np.dtype(dtype).itemsize
+    print("{:15,} {}s/sec ({})".format(int(size / t1), dtype, convert_bytes(int(size * nbytes / t1), suffix="B/s")))
